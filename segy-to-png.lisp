8d704cb6c95b9b4e0b55c1fed0b9991c272e5640
Øistein Haaland (IT SI SIS0) <oihaa@ai-linapp1125.statoil.no>
2019-07-29 11:12:18 +0000

    Fix

--- segy-to-png.lisp
+++ segy-to-png.lisp
@@ -26,8 +26,7 @@
 			 for elm = (row-major-aref line-buffer i )
 			 minimizing elm into min of-type single-float
 			 maximizing elm into max of-type single-float
-			      finally (return (list min max))))
-	     (min-max (list -10.0f0 40.0f0))
+			 finally (return (list min max))))
 	     (min-val (the single-float (first min-max)))
 	     (max-val (the single-float (second min-max)))
 	     (range (- max-val min-val))
@@ -40,38 +39,40 @@
 				    :palette *cmap*
 				    :width xlines
 				    :height samples)))
-	    (macrolet ((get-loc (i)
-			 `(+  (* (mod ,i xlines) samples) (truncate (/ ,i xlines)))))
-	      (zpng:start-png png out-stream)
-	      (loop for i below samples
-		 do (loop for j below xlines
-		       ;;			 for p = (get-loc i)
-		       do (zpng:write-pixel (list (scale (aref line-buffer i j )))  png)))
-	      (zpng:finish-png png))))))))
+	    (zpng:start-png png out-stream)
+	    (loop for i below samples
+	       do (loop for j below xlines
+		     do (zpng:write-pixel (list (scale (aref line-buffer i j )))  png)))
+	    (zpng:finish-png png)))))))
 
-#+nil (defun stream-image-xline (out-stream filename lineno decimation)
-  (segyio:with-xline (line-buffer filename lineno samples inlines decimation)
-    (let* ((inlines  (truncate (/ inlines decimation)))
-	   (size (* samples inlines)))
+(defun stream-image-xline (out-stream filename lineno decimation)
+  (segyio:with-inline (line-buffer filename lineno samples inlines decimation)
+    (declare (type fixnum inlines decimation samples))
+    (declare (type (simple-array single-float) line-buffer))
+    (let* ((inlines (the fixnum (truncate (/ inlines decimation))))
+	   (size (the fixnum (* samples  inlines))))
+      (declare (type fixnum inlines size))
       (let* ((min-max (loop for i below size
-			 for elm = (autowrap:c-aref line-buffer i :float)
-			 minimizing elm into min
-			 maximizing elm into max
+			 for elm = (row-major-aref line-buffer i )
+			 minimizing elm into min of-type single-float
+			 maximizing elm into max of-type single-float
 			 finally (return (list min max))))
-	     (min-val (first min-max))
-	     (max-val (second min-max))
-	     (factor (/ 255.0 (- max-val min-val))))
-	(flet ((scale (val)
-		 (floor (* (- val min-val) factor))))
+	     (min-val (the single-float (first min-max)))
+	     (max-val (the single-float (second min-max)))
+	     (range (- max-val min-val))
+	     (half-range (/ range 2.0f0))
+	     (factor (the single-float (/ 255.0 range))))
+	(macrolet ((scale (val)
+		     `(the fixnum (floor (min (max (* (+ ,val half-range) factor) 0.0f0) 255.0f0)))))
 	  (let ((png (make-instance 'zpng::pixel-streamed-png/palette
 				    :color-type :indexed-color
 				    :palette *cmap*
 				    :width inlines
 				    :height samples)))
-	    (flet ((get-loc (i)
-		     (+  (* (mod i inlines) samples) (truncate (/ i inlines)))))
-	      (zpng:start-png png out-stream)
-	      (loop for i below size
-		 for p = (get-loc i)
-		 do (zpng:write-pixel (list (scale (aref line-buffer p )))  png))
-	      (zpng:finish-png png))))))))     
+	    (zpng:start-png png out-stream)
+	    (loop for i below samples
+	       do (loop for j below inlines
+		     do (zpng:write-pixel (list (scale (aref line-buffer i j )))  png)))
+	    (zpng:finish-png png)))))))
+
+
